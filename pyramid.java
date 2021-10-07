429b3f1d955a91ac712821cfabcd5d880eeee0c1
modhtom <85494929+modhtom@users.noreply.github.com>
2021-10-07 17:40:15 +0200

    Add files via upload

---
+++ pyramid.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class pyramid{
+public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    System.out.print("Enter the number of rows:");
+    int r=input.nextInt();
+    for (int i=1;i<=r;++i) {
+    for (int j=1;j<=i;++j) {
+    System.out.print("* ");}
+    System.out.println();}
+}}
